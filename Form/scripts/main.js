86ca21d543d862bba406e90cbadf099437c5dd1b
Manu31240 <emmanuel.gavaldaorange.com>
2016-02-26 11:41:26 +0100

    commit vendredi

---
+++ Form/scripts/main.js
@@ -0,0 +1,7 @@
+require.config({
+// config options
+});
+
+//require(['jquery/jquery', 'common/ajaxLoader', 'common/someOtherModule'], function($, ajax, otherModule) {
+    // Modules that do stuff on every page are instantiated here 
+});
