c43c4990669077fd6aa663c15db7b54ae8b0073a
Manu31240 <emmanuel.gavaldaorange.com>
2016-03-10 16:26:22 +0100

    commit last

--- Form/scripts/main.js
+++ Form/scripts/main.js
@@ -2,17 +2,42 @@ require.config({
     baseUrl: 'scripts',
     paths: {
         'jquery': 'components/jquery/jquery'
-    },
-    shim:{
-        'apps2':['SessionIdWebcom']
     }
+});
+/*//require.config({
+//    baseUrl: 'scripts',
+//    paths: {
+//        'jquery': 'components/jquery/jquery'
+//    }
+    //,
+    //shim:{
+    //    'apps2':['SessionIdWebcom']
+    //}
 
     // config options
 });
+//
+//
+//
 //require(['jquery'],function() {});
+// main.js est chargé automatiquement grâce à
+// l'attribut data-main dans la balise script de require.js
+//require([
+//    'apps2', // scripts/app2.js
+//], function(App) {
+    // Callback executé une fois app.js chargé
+//    App.initialize();
+//});
 
+*/
+requirejs([
+"text!SS-Form.html" // app/templates/module.html
+]);
 
+/*    , function(SS-Form, html) {
+    // vous retrouvez ici votre template
+    // que vous pouvez compiler par exemple avec underscore
+    //var compiledTemplate = _.template(html);
+});
 
-
-
-
+*/
