dc32e500f493cd294e50f1fa08f7d284305f5b05
root <root@null-080027c80b54.toulouse.francetelecom.fr>
2016-04-19 09:53:28 +0200

    my first commit

---
+++ scripts/main.js
@@ -0,0 +1,18 @@
+require.config({
+    baseUrl: 'scripts',
+    paths: {
+        'jquery': 'components/jquery/jquery'
+    },
+    shim:{
+        'apps':['SessionIdWebcom']
+    }
+
+    // config options
+});
+//require(['jquery'],function() {});
+
+
+
+
+
+
