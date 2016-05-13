1171f421c0a022d75897018bf71ebf30e71f3b09
Manu31240 <emmanuel.gavalda@orange.com>
2016-05-13 14:41:28 +0200

    sauvegarde 1305

---
+++ Form/scripts/helloword.js
@@ -0,0 +1,4 @@
+/**
+ * Created by root on 5/13/16.
+ */
+console.log("Hello World");
--- Form/scripts/serveur.js
+++ Form/scripts/serveur.js
@@ -1,14 +1,14 @@
 /**
  * Created by root on 2/18/16.
  */
-var http = require('http');
+/*var http = require('http');
 var url = require('url');
 
 var server = http.createServer(function(req, res) {
     var page = url.parse(req.url).pathname;
     console.log(page);
     res.writeHead(200, {"Content-Type": "text/plain"});
-    if (page == '/root/Share/Form/') {
+    if (page == '/root/Webcom/Form/') {
         res.write('Vous êtes à l\'accueil, que puis-je pour vous ?');
     }
     else if (page == '/sous-sol') {
@@ -20,3 +20,19 @@ var server = http.createServer(function(req, res) {
     res.end();
 });
 server.listen(8080);
+*/
+var http = require("http");
+var Webcom = require("webcom");
+
+http.createServer(function(request, response) {
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+    var myRef = new Webcom('https://webcom.orange.com/base/form');
+    /*
+     Read data
+     */
+    myRef.on('child_added', function(snap){
+        console.info(snap.val());
+    });
+}).listen(8888);
